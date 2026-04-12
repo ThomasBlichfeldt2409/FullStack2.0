96edfb2f878a5902cc8d22af262054d3f943c493
ThomasBlichfeldt2409 <Thomas.Blichfeldt@specialisterne.dk>
2026-04-12 16:38:42 +0200

    Working on Login page

---
+++ Components/Pages/Login.razor.cs
@@ -0,0 +1,14 @@
+﻿namespace FullStack2._0.Components.Pages
+{
+    public partial class Login
+    {
+        protected string email = "";
+        protected string password = "";
+        protected string? errorMessage;
+
+        protected async Task HandleLogin()
+        {
+            errorMessage = "Du kan ikke logge på endnu";
+        }
+    }
+}
